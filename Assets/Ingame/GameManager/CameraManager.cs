52ce2149499481adbc747e549b6681b6e67a98df
GuN07 <75508131+gun07@users.noreply.github.com>
2024-11-14 19:48:14 +0900

    added gamehandler, some camera effects

---
+++ Assets/Ingame/GameManager/CameraManager.cs
@@ -0,0 +1,46 @@
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace GameManager
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public void MoveCamera(float x, float y, string easing, float dur)
+        {
+            StartCoroutine(MoveCameraToPosition(new Vector3(x, y, -10), easing, dur));
+        }
+
+        private IEnumerator MoveCameraToPosition(Vector3 endPos, string easing, float dur)
+        {
+            Vector3 startPos = Camera.main.transform.position;
+            float time = 0;
+            while (time < dur) 
+            {
+                Camera.main.transform.position = Vector3.Lerp(startPos, endPos, Easing.Ease(time / dur, easing));
+                time += Time.deltaTime;
+                yield return null;
+            }
+            Camera.main.transform.position = endPos;
+        }
+
+        public void ZoomCamera(float size, string easing, float dur)
+        {
+            StartCoroutine(ZoomCameraToSize(size, easing, dur));
+        }
+        
+        private IEnumerator ZoomCameraToSize(float endSize, string easing, float dur)
+        {
+            float startSize = Camera.main.orthographicSize;
+            float time = 0;
+            while (time < dur) 
+            {
+                Camera.main.orthographicSize = Easing.Ease(time / dur, easing) * (endSize - startSize) + startSize;
+                time += Time.deltaTime;
+                yield return null;
+            }
+            Camera.main.orthographicSize = endSize;
+        }
+    }
+}
