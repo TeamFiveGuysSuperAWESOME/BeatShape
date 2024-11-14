1c6874418f740f1f664cd0ac6c6f669a3dec0364
GuN07 <75508131+gun07@users.noreply.github.com>
2024-11-14 18:10:30 +0900

    fix multi input bug

--- Assets/Ingame/Beat/BeatMovement.cs
+++ Assets/Ingame/Beat/BeatMovement.cs
@@ -16,6 +16,9 @@ namespace Beat
         private readonly float _offset = 0f;
         private float _personalTimeOffset;
         private readonly float _rotationSpeed = -BeatboardManager.RotationSpeed;
+        private float elapsedTime;
+        private float sineValue;
+        private float adjustedSpeed;
 
         public void SetMovement(Vector2 dir, float spd, float bpm, Vector2 pos, float sze)
         {
@@ -27,8 +30,12 @@ namespace Beat
             _personalTimeOffset = Time.time;
         }
         
-        public void RemoveBeatScored()
+        public void TryRemoveBeatScored()
         {
+            if (Vector2.Distance(transform.position, _pos) > Math.Pow(_size, 1.3f) || sineValue > 0)
+            {
+                return;
+            }
             Destroy(gameObject);
             Debug.Log("Beat removed");
             MainGameManager.Score += (int)Math.Pow(_size, 1.3f) / (int)Vector2.Distance(transform.position, _pos);
@@ -37,9 +44,9 @@ namespace Beat
         private void Update()
         {
             float secondsPerBeat = 60f / _bpm;
-            float elapsedTime = Time.time - _personalTimeOffset;
-            float sineValue = Mathf.Sin(((elapsedTime / secondsPerBeat) * 0.5f * Mathf.PI * _spd) + _offset) * _amplitude;
-            float adjustedSpeed = _size * 3.5f * sineValue;
+            elapsedTime = Time.time - _personalTimeOffset;
+            sineValue = Mathf.Sin(((elapsedTime / secondsPerBeat) * 0.5f * Mathf.PI * _spd) + _offset) * _amplitude;
+            adjustedSpeed = _size * 3.5f * sineValue;
 
             transform.Translate(_direction * (adjustedSpeed * Time.deltaTime));
             transform.RotateAround(_pos, Vector3.forward, _rotationSpeed * Time.deltaTime);
